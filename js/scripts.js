f8547e9a7da77d5b316bb410d6307bb45bf40244
Kadirgu <Kadirgu@users.noreply.github.com>
2022-04-21 17:03:15 +0200

    change 21.april
    change 21.april

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,11 @@ let pokemonList= [
   {name: "Squirtle", type:"water", height:"1"}
 ]
 
+console.error('this Pokemon does not exist in the list')
+console.info('select your pokemon')
+console.log('regular message')
+
+
 for (let i=0; i < pokemonList.length; i++){
   if (pokemonList[i].height <1.5 && pokemonList[i].height >1){
     console.log(pokemonList[i].name + "this is a normal Pokemon");
@@ -17,5 +22,21 @@ for (let i=0; i < pokemonList.length; i++){
   }
 }
 
+
 let simpleAddition = 2 + 2;
 document.write(simpleAddition);
+
+
+function divide(dividend, divisor){
+  if(divisor === 0){
+    return "You’re trying to divide by zero."
+  }else{
+    let result = dividend / divisor;
+    return result;
+  }
+}
+
+console.log(divide(4, 2));
+console.log(divide(7, 0));
+console.log(divide(1, 4));
+console.log(divide(12, -3));
